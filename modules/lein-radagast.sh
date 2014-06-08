26eba3ac72c1c7276212f5f3e212787935be557f
arrdem <me@arrdem.com>
2014-06-08 01:37:58 -0500

    readability

--- modules/lein-radagast.sh
+++ modules/lein-radagast.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-grep -h -r "(ns" `git root`/test | awk "{print \$2}" | tr "\n" " " | xargs lein do clean, radagast
+grep -h -r "(ns" `git root`/test |\
+  awk "{print \$2}" |\
+  tr "\n" " " |\
+  xargs lein do clean, radagast
